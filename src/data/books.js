23ca855387980ecc65454250cbd5c4ace10ea35c
wanjiruaisha <wanjiruaisha30@gmail.com>
2026-06-28 11:48:53 +0300

    feat: add initial books dataset

---
+++ src/data/books.js
@@ -0,0 +1,34 @@
+export const initialBooks = [
+  {
+    id: 1,
+    title: "Atomic Habits",
+    author: "James Clear",
+    genre: "Self Improvement",
+    year: 2018,
+    rating: 5,
+  },
+  {
+    id: 2,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    genre: "Fiction",
+    year: 1988,
+    rating: 4,
+  },
+  {
+    id: 3,
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    genre: "Finance",
+    year: 1997,
+    rating: 4,
+  },
+  {
+    id: 4,
+    title: "Things Fall Apart",
+    author: "Chinua Achebe",
+    genre: "African Literature",
+    year: 1958,
+    rating: 5,
+  },
+];
